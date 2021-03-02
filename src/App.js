646807f6d7fea3975dff291ebaaac39e2b3a11d7
yulque <double.y.r@gmail.com>
2021-03-02 11:55:28 -0500

    detail added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Detail from './components/vaccination-detail/detail.component';
 
 function App() {
-  return <div className="App">Vassport</div>;
+  return (
+    <>
+      <div className="App">Vassport</div>
+      <Switch>
+        <Route path="/vaccination-detail">
+          <Detail />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
